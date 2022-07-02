72a3b097c55f4c6074f67bdde87116de40fef226
Kuro107 <gotovo56@gmail.com>
2022-07-02 16:26:28 +0600

    exam3.2

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,48 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) throws MyException {
-        List<Person> guys = new LinkedList<>(); // создаем LinkedList
+//  создаем LinkedList
+        List<Person> guys = new LinkedList<>();
+//  создаем объект класса person и добавляем в LinkedList что бы список не был пустым
+        Person person = new Person("A", "B", "C");
+        guys.add(person);
+//  вызываем метод для создания и добавления Person в LinkedList
+        addPersonToList(guys);
 
-        //создаем несколько Person
 
-        Person p = new Person("Za", "Za", "Za");
-        Person p1 = new Person("Za1", "Za2", "Za3");
-        Person p2 = new Person("Za", "Za", "Za");
-
-        //метод для добавления Person в LinkedList
-        //не стал изобретать велосипед и воспользовался готовым методом (add)
-
-        guys.add(p);
-        guys.add(p1);
-        guys.add(p2);
-
-        // при попадании дублирующихся Person в список - вызывать исключение
+        // при попадании дублирующихся Person в список - должен вызывать исключение
 
         for (int i = 0; i < guys.size(); i++) {
-            for (Object o : guys) {
-                if (guys.get(i).equals(o)) {
+            for (int j = 0; j< guys.size(); j++) {
+                if (guys.get(i).equals(guys.get(j))) {
                     throw new MyException("This person is already added");
                 }
             }
         }
     }
+
+        //метод для добавления Person в LinkedList
+
+    public static void addPersonToList (List<Person> guys) {
+        LinkedList<Person> somePerson = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = true;
+        while (exit) {
+            System.out.println("Введите ФИО персонажа которого хотите добавить");
+            somePerson.add(new Person(scanner.next(), scanner.next(), scanner.next()));
+            System.out.println("Для продолжения добавления нажмите 1, для выхода любую другую цифру ");
+            int s = scanner.nextInt();
+            if (s != 1){
+                guys.addAll(somePerson);
+                exit = false;
+            }
+
+        }
+
+    }
 }
+
